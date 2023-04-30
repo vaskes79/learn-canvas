cb0a0ce3ede78193649e1df16502d2779d0b321d
Vasily Guzov <vasily.guzov@gmail.com>
2023-04-30 22:32:09 +0300

    [ch2] examples draw lines

--- src/ch2/drawScreen.ts
+++ src/ch2/drawScreen.ts
@@ -4,26 +4,33 @@ export const drawScreen = (
   ctx: CanvasRenderingContext2D,
   canvasSettings: CanvasSettings
 ) => {
-  ctx.strokeStyle = "red";
-  // ctx.strokeStyle = "transparent";
-  ctx.lineWidth = 30;
-  // ctx.lineCap = "square";
+  // Sample 1: round end, bevel join, at top left of canvas
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 10;
+  ctx.lineJoin = "bevel";
   ctx.lineCap = "round";
-  ctx.lineJoin = "round";
   ctx.beginPath();
-  ctx.moveTo(20, 0);
-  ctx.lineTo(200, 0);
-  ctx.lineTo(200, 40);
-  ctx.lineTo(400, 40);
-  ctx.lineTo(400, 60);
-  ctx.lineTo(600, 60);
+  ctx.moveTo(0, 0);
+  ctx.lineTo(25, 0);
+  ctx.lineTo(25, 25);
   ctx.stroke();
   ctx.closePath();
-
-  ctx.fillStyle = "#000000";
-  ctx.font = "60px sans";
-  ctx.textBaseline = "top";
-  ctx.fillText("draw line", 0, 50);
+  // Sample 2: round end, bevel join, not at top or left of canvas
+  ctx.beginPath();
+  ctx.moveTo(10, 50);
+  ctx.lineTo(35, 50);
+  ctx.lineTo(35, 75);
+  ctx.stroke();
+  ctx.closePath();
+  // Sample 3: flat end, round join, not at top or left of canvas
+  ctx.lineJoin = "round";
+  ctx.lineCap = "butt";
+  ctx.beginPath();
+  ctx.moveTo(10, 100);
+  ctx.lineTo(35, 100);
+  ctx.lineTo(35, 125);
+  ctx.stroke();
+  ctx.closePath();
 
   console.log(canvasSettings);
 };
