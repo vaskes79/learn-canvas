e0ca38c0067ebc31339aa1dbcc6bfb7ab58f4a34
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-01 17:32:24 +0300

    [ch2] example using globalCompositOperation(), globalAlpha()

--- src/ch2/drawScreen.ts
+++ src/ch2/drawScreen.ts
@@ -5,31 +5,29 @@ export const drawScreen = (
   canvasSettings: CanvasSettings
 ) => {
   //draw a big box on the screen
-  ctx.fillStyle = "black";
+  ctx.fillStyle = "black"; //need list of available colors
   ctx.fillRect(10, 10, 200, 200);
-  ctx.save();
-  //clip the canvas to a 50×50 square starting at 0,0 ctx.rect(0, 0, 50, 50);
-  ctx.beginPath();
-  ctx.clip();
-  //red circle
-  ctx.beginPath();
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = 5;
-  ctx.arc(120, 120, 100, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false); //full circle
-  ctx.stroke();
-  ctx.closePath();
-  ctx.restore();
-  //reclip to the entire canvas
-  ctx.beginPath();
-  ctx.rect(0, 0, 500, 500);
-  ctx.clip();
-  //draw a blue line that is not clipped
-  ctx.beginPath();
-  ctx.strokeStyle = "blue";
-  ctx.lineWidth = 5;
-  ctx.arc(100, 100, 50, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false); //full circle
-  ctx.stroke();
-  ctx.closePath();
 
-  console.log(canvasSettings);
+  //leave golbal composite operation as is
+  //now draw a red square
+  ctx.fillStyle = "red"; //need list of available colors
+  ctx.fillRect(1, 1, 50, 50);
+
+  //now set it to source-over
+  ctx.globalCompositeOperation = "source-over";
+  //draw a red square next to the other one
+  ctx.fillRect(60, 1, 50, 50);
+
+  //now set to destination-atop
+  ctx.globalCompositeOperation = "destination-atop";
+  ctx.fillRect(1, 60, 50, 50);
+
+  //now set globalAlpha
+  ctx.globalAlpha = 0.5;
+
+  //now set to source-atop
+  ctx.globalCompositeOperation = "source-atop";
+  ctx.fillRect(60, 60, 50, 50);
+
+  // console.log(canvasSettings);
 };
