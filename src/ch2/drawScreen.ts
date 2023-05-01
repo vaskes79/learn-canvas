61ea2dab853b1a8a4bd13a9fe7f979e0b5626532
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-01 15:13:34 +0300

    [ch2] example using arcTo and func based on it

--- src/ch2/drawScreen.ts
+++ src/ch2/drawScreen.ts
@@ -1,20 +1,41 @@
 import { CanvasSettings } from "../types";
-import { drawArcsSquear } from "./drawArcsSquear";
+import { drawArcsSquare } from "./drawArcsSquare2";
 
 export const drawScreen = (
   ctx: CanvasRenderingContext2D,
   canvasSettings: CanvasSettings
 ) => {
-  drawArcsSquear(ctx, {
-    tileSize: 64,
+  drawArcsSquare(ctx, {
+    tileSize: 128,
     posX: 100,
-    posY: 100,
+    posY: 500,
+    color: "green",
+    segment: "topRight",
+    showDebug: true,
   });
 
-  drawArcsSquear(ctx, {
+  drawArcsSquare(ctx, {
     tileSize: 128,
-    posX: 500,
+    posX: 300,
+    posY: 100,
+    color: "red",
+    segment: "topLeft",
+    showDebug: true,
+  });
+
+  drawArcsSquare(ctx, {
+    tileSize: 128,
+    posX: 100,
+    posY: 100,
+    color: "blue",
+    segment: "bottomRight",
+  });
+
+  drawArcsSquare(ctx, {
+    tileSize: 128,
+    posX: 300,
     posY: 500,
+    color: "orange",
     segment: "bottomLeft",
   });
 
