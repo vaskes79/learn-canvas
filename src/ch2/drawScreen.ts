18b7356b26605792648d31356c5ca97de6ffc501
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-01 12:55:01 +0300

    [ch2] example using func for draw segment

--- src/ch2/drawScreen.ts
+++ src/ch2/drawScreen.ts
@@ -1,22 +1,22 @@
 import { CanvasSettings } from "../types";
+import { drawArckSquear } from "./drawArcksSquear";
 
 export const drawScreen = (
   ctx: CanvasRenderingContext2D,
   canvasSettings: CanvasSettings
 ) => {
-  ctx.beginPath();
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = 5;
-  // centerX,centerY,radius,startAngle,endEngle,anticlockwise is a true or false value that defines the direction of the arc.
-  // ctx.arc(100, 100, 95, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
-  // ctx.arc(100, 200, 20, (Math.PI / 180) * 0, (Math.PI / 180) * 90, false);
-  ctx.moveTo(0, 0);
-  ctx.lineTo(100, 200);
-  // page 43
-  ctx.arcTo(350, 350, 250, 350, 64);
-  //full circle
-  ctx.stroke();
-  ctx.closePath();
+  drawArckSquear(ctx, {
+    tileSize: 64,
+    posX: 100,
+    posY: 100,
+  });
+
+  drawArckSquear(ctx, {
+    tileSize: 128,
+    posX: 500,
+    posY: 500,
+    segment: "bottomLeft",
+  });
 
   console.log(canvasSettings);
 };
