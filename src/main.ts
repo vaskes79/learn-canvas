f52dae03dd31f83c7fd5a402815654f7c95b79a1
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-06 17:35:02 +0300

    [clear] setup for new project

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,14 @@
 import "./style.css";
 import styles from "./Game.module.css";
-import { initCh2 } from "./ch2";
+import { Game as DragAndDropGame } from "./DragAnDrop";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div id="game-container" class="${styles.gameContainer}">
-    <canvas id="canvas"></canvas>
   </div>
 `;
 
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-// const W = (canvas.width = canvas.clientWidth * devicePixelRatio);
-// const H = (canvas.height = canvas.clientHeight * devicePixelRatio);
-const W = (canvas.width = canvas.clientWidth);
-const H = (canvas.height = canvas.clientHeight);
-const ctx = canvas.getContext("2d");
-const canvasSettings = {
-  W,
-  H,
-};
+const gameContainer = document.getElementById("game-container");
 
-if (ctx) {
-  initCh2(ctx, canvasSettings);
+if (gameContainer) {
+  new DragAndDropGame(gameContainer);
 }
