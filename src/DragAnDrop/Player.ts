7aca4ea2a443175fa2929a8e6c8844080e364fcf
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-06 17:37:05 +0300

    [dnd] draft implementation draw player layer

--- src/DragAnDrop/Player.ts
+++ src/DragAnDrop/Player.ts
@@ -1,93 +1,43 @@
+import { getRandomNumber, getRandomColor, generateItems } from "./utils";
 import type { KeyboardControls } from "./KeyboardControls";
 import type { Layer } from "./Layer";
 import type { MouseControls } from "./MouseControls";
+import { Squere } from "./Square";
 
 export class Player {
-  options: {
-    x: number;
-    y: number;
-    w: number;
-    h: number;
-    vx: number;
-    vy: number;
-    minH: number;
-    maxH: number;
-    gravity: number;
-    color: string;
-    isCrouch: boolean;
-    isAir: boolean;
-  };
+  private _itemsList: Squere[];
 
   constructor(
     private _layer: Layer,
     private _mouse: MouseControls,
     private _keyboard: KeyboardControls
   ) {
-    this.options = {
-      x: 0,
-      y: 0,
-      w: 32,
-      h: 64,
-      vx: 700,
-      vy: 0,
-      minH: 32,
-      maxH: 64,
-      isAir: true,
-      color: `orange`,
-      gravity: 90,
-      isCrouch: false,
-    };
-    console.log(this._mouse);
+    this._layer.canvas.style.zIndex = "2";
+    const squerSize = 128;
+    const numberOfItems = 10;
+    this._itemsList = generateItems<Squere>(
+      numberOfItems,
+      () =>
+        new Squere(
+          getRandomNumber(0, this._layer.W - squerSize),
+          getRandomNumber(0, this._layer.H - squerSize),
+          squerSize,
+          getRandomColor()
+        )
+    );
   }
 
   update = (correction: number) => {
-    if (this._keyboard.keys.KeyS && this.options.h > this.options.minH) {
-      this.options.h -= 3;
-      this.options.y += 3;
-      this.options.isCrouch = true;
-    }
-    if (!this._keyboard.keys.KeyS && this.options.h < this.options.maxH) {
-      this.options.h += 3;
-      this.options.isCrouch = false;
-    }
-
-    if (this._keyboard.keys.KeyD) {
-      this.options.x += this.options.vx * correction;
-    }
-    if (this._keyboard.keys.KeyA) {
-      this.options.x -= this.options.vx * correction;
-    }
-
-    if (
-      this._keyboard.keys.Space &&
-      !this.options.isAir &&
-      !this.options.isCrouch
-    ) {
-      this.options.vy = -30;
-      this.options.isAir = true;
-    }
-
-    if (this.options.isAir) {
-      this.options.vy += this.options.gravity * correction;
-    } else {
-      this.options.vy = 0;
-    }
-    this.options.y += this.options.vy;
-
-    if (this.options.y + this.options.h >= this._layer.H) {
-      this.options.y = this._layer.H - this.options.h;
-      this.options.isAir = false;
-    }
+    // this._itemsList.forEach((item) => {
+    //   item.display(this._layer.ctx);
+    // });
   };
 
   display = () => {
     this._layer.ctx.clearRect(0, 0, this._layer.W, this._layer.H);
-    this._layer.ctx.fillStyle = this.options.color;
-    this._layer.ctx.fillRect(
-      this.options.x,
-      this.options.y,
-      this.options.w,
-      this.options.h
-    );
+    this._layer.ctx.fillRect(200, 200, 128, 128);
+    // this._itemsList.forEach((item) => {
+    //   item.display(this._layer.ctx);
+    // });
   };
 }
