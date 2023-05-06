c8c4d734607037e17e314621ca0226c9037140ea
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-06 21:51:54 +0300

    [dnd] 0.0.1 version

--- src/DragAnDrop/Player.ts
+++ src/DragAnDrop/Player.ts
@@ -12,6 +12,7 @@ import { Squere } from "./Square";
 
 export class Player {
   private _itemsList: Squere[];
+  private _selectedItem: Squere | null;
 
   constructor(
     private _layer: Layer,
@@ -19,6 +20,7 @@ export class Player {
     private _keyboard: KeyboardControls
   ) {
     this._layer.canvas.style.zIndex = "2";
+    this._selectedItem = null;
     const squerSize = 64;
     const numberOfItems = 10;
     this._itemsList = generateItems<Squere>(
@@ -34,31 +36,44 @@ export class Player {
   }
 
   update = () => {
-    const { position, isPressed, isDown } = this._mouse;
-
-    if (position.x && position.y) {
+    const { position, isPressed, isUp, isDown } = this._mouse;
+    if (!this._selectedItem) {
       this._itemsList.forEach((item) => {
         const isActive = cursorInRect(position, item);
-        const isSelected = isActive && isPressed;
-        item.activate(isActive);
-        item.select(isSelected);
-
-        if (isDown && isActive) {
-          item.offset = getOffsetCoords(position, item);
-        }
-
-        if (
-          isSelected &&
-          position.x &&
-          position.y &&
-          item.offset.x &&
-          item.offset.y
-        ) {
-          item.x = position.x - item.offset.x;
-          item.y = position.y - item.offset.y;
+        if (isActive) {
+          item.activate(true);
+        } else {
+          item.activate(false);
         }
         item.display(this._layer.ctx);
       });
+    }
+    if (isDown) {
+      this._itemsList.forEach((item) => {
+        const isActive = cursorInRect(position, item);
+        if (isActive) {
+          item.activate(true);
+          item.select(true);
+          item.offset = getOffsetCoords(position, item);
+          this._selectedItem = item;
+        }
+        item.display(this._layer.ctx);
+      });
+    }
+
+    if (isPressed && position.x && position.y && this._selectedItem) {
+      const item = this._selectedItem;
+      if (position.x && position.y && item.offset.x && item.offset.y) {
+        item.x = position.x - item.offset.x;
+        item.y = position.y - item.offset.y;
+      }
+    }
+
+    if (isUp && this._selectedItem) {
+      this._selectedItem.activate(false);
+      this._selectedItem.select(false);
+      this._selectedItem.offset = { x: 0, y: 0 };
+      this._selectedItem = null;
     }
   };
 
