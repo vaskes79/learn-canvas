d0c7dca11ab01dc216a3cf5838acb26286cbfa8d
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-06 17:35:30 +0300

    [utils] for color and generateItems

--- src/DragAnDrop/utils/utils.ts
+++ src/DragAnDrop/utils/utils.ts
@@ -16,3 +16,11 @@ export function incrementAngle(angle: number) {
   }
   return angle;
 }
+
+export function getRandomNumber(min: number = 0, max: number = 100) {
+  return Math.random() * (max - min) + min;
+}
+
+export function generateItems<T>(numberOfItems: number, callback: () => T) {
+  return new Array(numberOfItems).fill(numberOfItems).map(callback);
+}
