bea24b757a4ec929914819bfc53a742a6237b6ce
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-06 16:03:03 +0300

    [dnd][Layer] correct devicePixelRatio

--- src/DragAnDrop/Layer.ts
+++ src/DragAnDrop/Layer.ts
@@ -3,11 +3,13 @@ export class Layer {
   ctx: CanvasRenderingContext2D;
   W: number = 0;
   H: number = 0;
+  _dp: number;
 
   constructor(container: HTMLElement) {
     this.canvas = document.createElement(`canvas`);
     this.ctx = this.canvas.getContext(`2d`) as CanvasRenderingContext2D;
     container.appendChild(this.canvas);
+    this._dp = devicePixelRatio;
 
     this.mount();
     this.fitToContainer();
@@ -22,7 +24,10 @@ export class Layer {
   };
 
   fitToContainer = () => {
-    this.W = this.canvas.width = this.canvas.clientWidth;
-    this.H = this.canvas.height = this.canvas.clientHeight;
+    this.W = this.canvas.width = Math.floor(this.canvas.clientWidth * this._dp);
+    this.H = this.canvas.height = Math.floor(
+      this.canvas.clientHeight * this._dp
+    );
+    this.ctx.scale(this._dp, this._dp);
   };
 }
