968ecf3af583f4692c62af3118975a98e527fe65
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-06 18:08:48 +0300

    [dnd][Bg] correct mesure lines

--- src/DragAnDrop/Bg.ts
+++ src/DragAnDrop/Bg.ts
@@ -5,32 +5,26 @@ export class Bg {
 
   constructor(private _layer: Layer) {
     this.options = {};
-    this._layer.canvas.style.position = "absolute";
-    this._layer.canvas.style.zIndex = "1";
   }
 
   display = () => {
-    const { W, H, ctx } = this._layer;
-    ctx.clearRect(0, 0, W, H);
-    ctx.lineWidth = 2;
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.font = "10px Arial";
+    const { sW, sH, ctx } = this._layer;
+    ctx.clearRect(0, 0, sW, sH);
 
     ctx.save();
 
     ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, W, H);
+    ctx.fillRect(0, 0, sW, sH);
     ctx.lineWidth = 0.3;
     ctx.strokeStyle = "lightgray";
     ctx.fillStyle = "black";
 
     // Draw grid
-    for (let i = 1; i < W; i++) {
+    for (let i = 1; i < sW; i++) {
       ctx.beginPath();
       if (i % 10 === 0) {
         ctx.moveTo(i, 0);
-        ctx.lineTo(i, H);
+        ctx.lineTo(i, sH);
         // todo: in original solution. I don't understand WHY?
         // ctx.moveTo(i, 0);
       }
@@ -38,11 +32,11 @@ export class Bg {
       ctx.stroke();
     }
 
-    for (let i = 1; i < H; i++) {
+    for (let i = 1; i < sH; i++) {
       ctx.beginPath();
       if (i % 10 === 0) {
         ctx.moveTo(0, i);
-        ctx.lineTo(W, i);
+        ctx.lineTo(sW, i);
         // todo: in original solution. I don't understand WHY?
         // ctx.moveTo(0, i);
       }
@@ -50,14 +44,19 @@ export class Bg {
       ctx.stroke();
     }
 
+    ctx.lineWidth = 2;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = "10px Arial";
+    const sizeNumberLine = 20;
     // Draw mesure lines
     ctx.lineWidth = 1;
     ctx.strokeStyle = "gray";
     ctx.beginPath();
-    for (let i = 50; i < W; i += 10) {
+    for (let i = 50; i < sW; i += 10) {
       if (i % 50 === 0) {
         ctx.moveTo(i, 0);
-        ctx.lineTo(i, 30);
+        ctx.lineTo(i, sizeNumberLine);
         ctx.fillText(` ${i}`, i, 30);
       } else {
         ctx.moveTo(i, 0);
@@ -68,10 +67,10 @@ export class Bg {
     ctx.stroke();
 
     ctx.beginPath();
-    for (let i = 50; i < H; i += 10) {
+    for (let i = 50; i < sH; i += 10) {
       if (i % 50 === 0) {
         ctx.moveTo(0, i);
-        ctx.lineTo(30, i);
+        ctx.lineTo(sizeNumberLine, i);
         ctx.fillText(` ${i}`, 30, i);
       } else {
         ctx.moveTo(0, i);
@@ -80,7 +79,6 @@ export class Bg {
     }
     ctx.closePath();
     ctx.stroke();
-
     ctx.restore();
   };
 }
