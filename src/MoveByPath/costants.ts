ff73caa0815236fe802de14c3648fae09d8d3901
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-07 19:34:14 +0300

    [mock] add mock data for crete rounded coreners

--- src/MoveByPath/costants.ts
+++ src/MoveByPath/costants.ts
@@ -28,19 +28,25 @@ export const mockPointsSquere: Point[] = [
   },
 ];
 
-export const mockPoints: Point[] = [
+export const mockPointsRoundCorender: Point[] = [
+  {
+    x: 128 * 2,
+    y: 128,
+    type: "horizontal",
+    direction: "right",
+  },
   {
     x: 128 * 3,
     y: 128,
     type: "horizontal",
     direction: "right",
   },
-  // {
-  //   x: 128 * 3,
-  //   y: 128 * 3,
-  //   type: "bottomLeft",
-  //   direction: "bottom",
-  // },
+  {
+    x: 128 * 3,
+    y: 128,
+    type: "bottomLeft",
+    direction: "bottom",
+  },
   {
     x: 128 * 3,
     y: 128 * 2,
@@ -55,20 +61,32 @@ export const mockPoints: Point[] = [
   },
   {
     x: 128 * 3,
-    y: 128 * 4,
+    y: 128 * 3,
     type: "topLeft",
     direction: "bottom",
   },
   {
     x: 128 * 2,
-    y: 128 * 4,
+    y: 128 * 3,
     type: "horizontal",
     direction: "left",
   },
   {
     x: 128,
-    y: 128 * 4,
-    type: "horizontal",
-    direction: "left",
+    y: 128 * 3,
+    type: "topRight",
+    direction: "top",
+  },
+  {
+    x: 128,
+    y: 128 * 2,
+    type: "vertical",
+    direction: "top",
+  },
+  {
+    x: 128,
+    y: 128,
+    type: "vertical",
+    direction: "top",
   },
 ];
