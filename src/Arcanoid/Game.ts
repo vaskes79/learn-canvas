927f355464cd4e8a609853afce81fc1e8478e465
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-08 11:10:08 +0300

    [arkanoid] add text layer

--- src/Arcanoid/Game.ts
+++ src/Arcanoid/Game.ts
@@ -5,20 +5,23 @@ import { Loop } from "./Loop";
 import { MouseControls } from "./MouseControls";
 import { Player } from "./Player";
 import { KeysPlayer } from "./costants";
+import { Text } from './Text'
 
 export class Game {
   player: Player;
   bg: Bg;
+  text: Text;
   mouse: MouseControls;
   loopControls: Loop;
   keyboard: KeyboardControls;
 
   constructor(private _container: HTMLElement) {
     const bgLayer = new Layer(this._container, 1);
-    bgLayer.canvas.setAttribute("id", "bg");
-    const playerLayer = new Layer(this._container, 2);
+    const textLayer = new Layer(this._container, 2);
+    const playerLayer = new Layer(this._container, 3);
     this.mouse = new MouseControls(this._container);
     this.keyboard = new KeyboardControls(KeysPlayer);
+    this.text = new Text(textLayer);
     this.player = new Player(playerLayer, this.mouse, this.keyboard);
     this.bg = new Bg(bgLayer);
 
@@ -33,6 +36,8 @@ export class Game {
   display = () => {
     this.player.display();
     this.bg.display();
+    this.text.score = 0;
+    this.text.display()
   };
 
   // setSpeedPlayer = (speed: number) => {
---
+++ src/Arcanoid/Text.ts
@@ -0,0 +1,16 @@
+import { Layer } from "./Layer";
+
+export class Text {
+  score: number = 0;
+  x: number = 32;
+  y: number = 32 + 16;
+
+  constructor(private _layer: Layer) {
+    this._layer.ctx.font = "20px Arial";
+    this._layer.ctx.fillStyle = "#fff";
+  }
+
+  display = () => {
+    this._layer.ctx.fillText("Score: " + this.score, this.x, this.y);
+  }
+}
